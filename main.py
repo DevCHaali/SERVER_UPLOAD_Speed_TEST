46d6c1234ae2374399a5bc2b45d9cb7d2d0f9e2a
HC Hacker <129988782+DevCHaali@users.noreply.github.com>
2024-08-09 07:55:00 -0700

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
-import os
 
 app = Flask(__name__)
-CORS(app)  # تفعيل CORS لجميع المسارات
+CORS(app)  # Enable CORS for all routes
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
@@ -34,4 +34,7 @@ def upload_file():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True, port=0000)
+
+    # Set the port dynamically from an environment variable or use 5000 as the default
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
