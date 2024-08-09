6b19b8a9a7bf84a0976282c95607d6b8472851c6
HC Hacker <129988782+DevCHaali@users.noreply.github.com>
2024-08-09 03:44:26 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+CORS(app)  # تفعيل CORS لجميع المسارات
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
+
+def allowed_file(filename):
+    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif', 'pdf','py'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+@app.route('/')
+def hello():
+    return "<h1> Hello , this is api web for test upload .</h1>"
+@app.route('/upload', methods=['POST'])
+
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'File uploaded successfully', 200
+    else:
+        return 'Invalid file type', 400
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
